a210526cb49a4e403b73d4519af0aed959014eda
RASHED FARUQUE <23-52924-2@student.aiub.edu>
2025-06-28 22:28:07 +0600

    register shows print

--- IchirakuRamenShop/Form2.Designer.cs
+++ IchirakuRamenShop/Form2.Designer.cs
@@ -32,17 +32,17 @@
             panel1 = new Panel();
             pictureBox1 = new PictureBox();
             label1 = new Label();
-            textBox1 = new TextBox();
+            txtName = new TextBox();
             label3 = new Label();
-            textBox2 = new TextBox();
+            txtUsername = new TextBox();
             label4 = new Label();
-            textBox3 = new TextBox();
-            button1 = new Button();
-            button2 = new Button();
+            txtPassword = new TextBox();
+            btnRegister = new Button();
+            btnBack = new Button();
             imageList1 = new ImageList(components);
             label2 = new Label();
             label5 = new Label();
-            textBox4 = new TextBox();
+            txtPhone = new TextBox();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
@@ -54,16 +54,18 @@
             panel1.Controls.Add(label1);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(670, 204);
+            panel1.Size = new Size(515, 159);
             panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources._1265390;
-            pictureBox1.Location = new Point(222, 12);
+            pictureBox1.Location = new Point(171, 9);
+            pictureBox1.Margin = new Padding(2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(173, 146);
+            pictureBox1.Size = new Size(133, 114);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 11;
             pictureBox1.TabStop = false;
@@ -74,78 +76,86 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
             label1.ForeColor = SystemColors.ActiveCaptionText;
-            label1.Location = new Point(12, 161);
+            label1.Location = new Point(9, 126);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(400, 37);
+            label1.Size = new Size(295, 28);
             label1.TabIndex = 0;
             label1.Text = "Please Enter Your Information";
             // 
-            // textBox1
+            // txtName
             // 
-            textBox1.Location = new Point(104, 276);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(467, 39);
-            textBox1.TabIndex = 2;
-            textBox1.TextChanged += textBox1_TextChanged;
+            txtName.Location = new Point(80, 216);
+            txtName.Margin = new Padding(2);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(360, 31);
+            txtName.TabIndex = 2;
+            txtName.TextChanged += textBox1_TextChanged;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(104, 335);
+            label3.Location = new Point(80, 262);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(121, 32);
+            label3.Size = new Size(91, 25);
             label3.TabIndex = 3;
             label3.Text = "Username";
             // 
-            // textBox2
+            // txtUsername
             // 
-            textBox2.Location = new Point(104, 380);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(467, 39);
-            textBox2.TabIndex = 4;
+            txtUsername.Location = new Point(80, 297);
+            txtUsername.Margin = new Padding(2);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(360, 31);
+            txtUsername.TabIndex = 4;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(104, 449);
+            label4.Location = new Point(80, 351);
+            label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new Size(111, 32);
+            label4.Size = new Size(87, 25);
             label4.TabIndex = 5;
             label4.Text = "Password";
             label4.Click += label4_Click;
             // 
-            // textBox3
+            // txtPassword
             // 
-            textBox3.Location = new Point(104, 499);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(467, 39);
-            textBox3.TabIndex = 6;
+            txtPassword.Location = new Point(80, 390);
+            txtPassword.Margin = new Padding(2);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(360, 31);
+            txtPassword.TabIndex = 6;
             // 
-            // button1
+            // btnRegister
             // 
-            button1.BackColor = Color.Orange;
-            button1.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            button1.ForeColor = SystemColors.ButtonHighlight;
-            button1.Location = new Point(76, 716);
-            button1.Name = "button1";
-            button1.Size = new Size(195, 61);
-            button1.TabIndex = 9;
-            button1.Text = "Register";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
+            btnRegister.BackColor = Color.Orange;
+            btnRegister.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            btnRegister.ForeColor = SystemColors.ButtonHighlight;
+            btnRegister.Location = new Point(58, 559);
+            btnRegister.Margin = new Padding(2);
+            btnRegister.Name = "btnRegister";
+            btnRegister.Size = new Size(150, 48);
+            btnRegister.TabIndex = 9;
+            btnRegister.Text = "Register";
+            btnRegister.UseVisualStyleBackColor = false;
+            btnRegister.Click += button1_Click;
             // 
-            // button2
+            // btnBack
             // 
-            button2.BackColor = Color.Orange;
-            button2.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            button2.ForeColor = SystemColors.ButtonHighlight;
-            button2.Location = new Point(348, 716);
-            button2.Name = "button2";
-            button2.Size = new Size(195, 61);
-            button2.TabIndex = 10;
-            button2.Text = "Back";
-            button2.UseVisualStyleBackColor = false;
-            button2.Click += button2_Click;
+            btnBack.BackColor = Color.Orange;
+            btnBack.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            btnBack.ForeColor = SystemColors.ButtonHighlight;
+            btnBack.Location = new Point(268, 559);
+            btnBack.Margin = new Padding(2);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(150, 48);
+            btnBack.TabIndex = 10;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = false;
+            btnBack.Click += button2_Click;
             // 
             // imageList1
             // 
@@ -156,9 +166,10 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(104, 230);
+            label2.Location = new Point(80, 180);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(78, 32);
+            label2.Size = new Size(59, 25);
             label2.TabIndex = 1;
             label2.Text = "Name";
             label2.Click += label2_Click;
@@ -166,36 +177,39 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(104, 563);
+            label5.Location = new Point(80, 440);
+            label5.Margin = new Padding(2, 0, 2, 0);
             label5.Name = "label5";
-            label5.Size = new Size(82, 32);
+            label5.Size = new Size(62, 25);
             label5.TabIndex = 11;
             label5.Text = "Phone";
             // 
-            // textBox4
+            // txtPhone
             // 
-            textBox4.Location = new Point(104, 598);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(467, 39);
-            textBox4.TabIndex = 12;
+            txtPhone.Location = new Point(80, 467);
+            txtPhone.Margin = new Padding(2);
+            txtPhone.Name = "txtPhone";
+            txtPhone.Size = new Size(360, 31);
+            txtPhone.TabIndex = 12;
             // 
             // Form2
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(670, 860);
-            Controls.Add(textBox4);
+            ClientSize = new Size(515, 672);
+            Controls.Add(txtPhone);
             Controls.Add(label5);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(textBox3);
+            Controls.Add(btnBack);
+            Controls.Add(btnRegister);
+            Controls.Add(txtPassword);
             Controls.Add(label4);
-            Controls.Add(textBox2);
+            Controls.Add(txtUsername);
             Controls.Add(label3);
-            Controls.Add(textBox1);
+            Controls.Add(txtName);
             Controls.Add(label2);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(2);
             Name = "Form2";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "sx";
@@ -210,17 +224,17 @@
 
         private Panel panel1;
         private Label label1;
-        private TextBox textBox1;
+        private TextBox txtName;
         private Label label3;
-        private TextBox textBox2;
+        private TextBox txtUsername;
         private Label label4;
-        private TextBox textBox3;
-        private Button button1;
-        private Button button2;
+        private TextBox txtPassword;
+        private Button btnRegister;
+        private Button btnBack;
         private ImageList imageList1;
         private PictureBox pictureBox1;
         private Label label2;
         private Label label5;
-        private TextBox textBox4;
+        private TextBox txtPhone;
     }
 }
